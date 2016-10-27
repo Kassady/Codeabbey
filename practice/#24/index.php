2c5c6ef0c04e3b164df18c248f5b56457d6a37b9
Max <m.peshehonov@maximaster.ru>
2016-10-27 14:42:31 +0300

    #24

--- practice/#24/index.php
+++ practice/#24/index.php
@@ -3,16 +3,47 @@ $buffer = fopen("input.txt","r+");
 $count = fgets($buffer);
 $values = fgets($buffer);
 $values = explode(' ',$values);
-$counter = 0;
-for ($i = 0; $i < $count; $i++) {
-    $values[$i] = $values[$i]**2;
-    if(strlen($values[$i])<8) {
-        $dif = 8 - strlen($values[$i]);
-        for ($j=0;$j<$dif;$j++){
-            $values[$i] =(int) (0 . $values[$i]);
+function fonNeumann($value) {
+    $value = $value**2;
+    $length = strlen($value);
+    if ($length < 8) {
+        $dif = 8 - $length;
+        $value = moreSymbols($value, $dif);
+    }
+    $value = ($value/100)%10000;
+    $length = strlen($value);
+    if ($length < 4) {
+        $dif = 4 - $length;
+        $value = moreSymbols($value,$dif);
+    }
+
+    return $value;
+}
+function moreSymbols($string,$rep) {
+    for ($k = 0; $k<$rep; $k++) {
+        $string = 0 . $string;
+    }
+    return $string;
+}
+function looping($number) {
+    $counter = 0;
+    $stack = [];
+    array_push($stack,$number);
+    $Match = false;
+    $i = 0;
+    while (!$Match) {
+        $new = fonNeumann($stack[$i]);
+        $i++;
+        $counter++;
+        if (in_array($new,$stack)) {
+            $Match = true;
+        }
+        else {
+            array_push($stack, $new);
         }
     }
-    $values[$i] = ($values[$i]/100)%1000;
-    var_dump($values[$i]);
+    return $counter;
 }
-
+for ($i = 0; $i < $count; $i++) {
+    echo looping($values[$i]) . " ";
+}
